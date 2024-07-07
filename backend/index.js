82b0b25ba6384e30c281afd643c594b402fdc4fc
Yogesh-dev318 <yogespoonia2020@gmail.com>
2024-07-08 01:25:24 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors=require("cors")
+const app=express()
+app.use(cors())
+app.use(express.json())
+const mainrouter=require("./routes/index")
+
+
+
+app.use("/api/v1",mainrouter)
+
+
+app.listen(3000)
